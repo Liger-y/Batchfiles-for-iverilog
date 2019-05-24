4e107d402f9fbde4b85d254518e3a611571e1d9b
Liger <ngxiao103@126.com>
2019-05-24 21:41:15 +0800

    add recompile
    compile function update

--- _lib/_A2Z.bat
+++ _lib/_A2Z.bat
@@ -1,18 +1,36 @@
 @echo off
+color 0f
+
+:start
 set "p=%~dp0"
 for /f "delims=\" %%i in ("%p:~0,-1%") do (set PrjName=%%~ni)
 
 dir *.v /b>file_list.txt
 
-@echo on
 iverilog -o %PrjName% -c file_list.txt
-@pause
+if %errorlevel% == 0 (
+    echo Compile successfully!
+) else (
+    echo -------------------------------
+    echo           Fix that
+    echo             and
+    echo  Press Enter to compile again.
+    echo -------------------------------
+    pause
+    cls
+    goto start
+)
 
-@del .\test.txt
+if exist .\test.txt (
+    del .\test.txt
+)
 vvp -n %PrjName% -lxt2
-@pause
+echo -------------------------------
+echo  Press Enter to run GTKWave.
+echo -------------------------------
+pause
 
-@if exist *.lxt2 (del .\*.lxt2)
-@ren %~dp0"test.vcd" wave.lxt2
+if exist *.lxt2 (del .\*.lxt2)
+ren %~dp0"test.vcd" wave.lxt2
 
 gtkwave wave.lxt2
